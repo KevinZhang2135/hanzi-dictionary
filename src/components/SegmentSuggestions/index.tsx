df5fdaec1310ef6471dd047483a8d7f6c4865136
Kevin <kevinbczhang@gmail.com>
2025-07-27 21:28:21 -0400

    Smoother Animations

--- src/components/SegmentSuggestions/index.tsx
+++ src/components/SegmentSuggestions/index.tsx
@@ -9,16 +9,17 @@ const SegmentSuggestions = (props: {
   if (!isDisplayed) return;
 
   return (
-    <div className="flex flex-nowrap gap-2 overflow-x-auto 
+    <div className="flex flex-nowrap gap-2 overflow-x-auto overflow-y-hidden
         text-base md:text-lg text-zinc-100 text-nowrap"
     >
       {segments.map((segment, index) => {
         return (
           <button
             key={`segment-suggestion-${segment}-${index}`}
-            className="px-2 py-1 bg-zinc-950 rounded-md
-              animate-appear transition-color duration-300
-              hover:bg-rose-500 active:bg-rose-600"
+            className="px-2 py-1 bg-zinc-950 rounded-md animate-appear
+              transition ease-in-out duration-300 delay-100
+              hover:scale-105 hover:bg-rose-500
+              active:bg-rose-600"
             onClick={() => enterSearchTerm(segment)}
           >
             {segment}
